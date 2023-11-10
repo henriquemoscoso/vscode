8b14258a1d6bc5bcc0c5697fc6cb4b0412fcdbb4
henrique moscoso <henriquemoscoso@gmail.com>
2023-11-10 13:18:42 -0300

    add coment

--- banheiros.py
+++ banheiros.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 planilha_df = pd.read_csv("D:/Users/henrique.moscoso/data.vs/Banheiro+sabonete.csv")                    # "D:/Users/henrique.moscoso/data.vs/ : endereço dell
 planilha_df.to_csv('teste.csv')                                                                         
-
+#suarez
 planilha_dic = planilha_df.to_dict('list')            # DataFrame para dicionário
 
 data_sel = input("Qual data deseja verificar o consumo? Separe por / (i.e 01/09/2009): ")
